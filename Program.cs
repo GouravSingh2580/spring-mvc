dcaaf0158db747f06790f2872bafff25f3d543ba
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2023-03-21 09:56:39 +0000

    calci made

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a mathematical expression:");
+        string input = Console.ReadLine();
+        
+        try
+        {
+            double result = Evaluate(input);
+            Console.WriteLine("Result: " + result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+    static double Evaluate(string expression)
+    {
+        System.Data.DataTable table = new System.Data.DataTable();
+        return Convert.ToDouble(table.Compute(expression, ""));
+    }
+}
